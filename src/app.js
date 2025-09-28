3af388ddd83abfb31cc87b04e252ee24ba24c0ff
mzeeshanaltaf <zeeshan.altaf@gmail.com>
2025-09-28 19:41:41 +0500

    Initial version

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: "./.env",
+})
+
+const app = express()
+
+// Basic configurations
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended:true, limit: "16kb"}))
+app.use(express.static("public"))
+
+// CORS Configuration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Authorization", "Content-Type"]
+}))
+
+// Import the routes
+import healthCheckRouter from "./routes/healthcheck.routes.js"
+import authRouter from "./routes/auth.routes.js"
+
+app.use("/api/v1/healthcheck", healthCheckRouter)
+app.use("/api/v1/auth", authRouter)
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Basecampy!")
+})
+
+export default app;
