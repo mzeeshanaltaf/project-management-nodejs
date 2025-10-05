9f55a91579cd5ef15575dbf12d3a052d35da7593
mzeeshanaltaf <zeeshan.altaf@gmail.com>
2025-10-05 19:45:30 +0500

    Added auth middleware code

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
 dotenv.config({
     path: "./.env",
@@ -12,6 +13,7 @@ const app = express()
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended:true, limit: "16kb"}))
 app.use(express.static("public"))
+app.use(cookieParser())
 
 // CORS Configuration
 app.use(cors({
--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -69,6 +69,51 @@ const registerUser = asyncHandler(async (req, res) => {
     return res.status(201).json(new ApiResponse(200, {user: createdUser}, "User registered successfully and verification email has been sent on your email"))
 })
 
-export {registerUser}
+const login = asyncHandler(async(req, res) => {
+    const {email, password, username} = req.body
+
+    if(!email){
+        throw new ApiError(400, "Email is required")
+    }
+
+    const user = await User.findOne({email})
+
+    if(!user){
+        throw new ApiError(400, "User does not exists")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(400, "Invalid Credentials")
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+    const loggedIndUser = await User.findById(user._id).select(
+    "-password -refreshToken -emailVerificationToken -emailVerificationExpiry"
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: loggedIndUser,
+                    accessToken,
+                    refreshToken
+                },
+                "User logged in successfully"
+            )
+        )
+})
+export {registerUser, login}
 
 
